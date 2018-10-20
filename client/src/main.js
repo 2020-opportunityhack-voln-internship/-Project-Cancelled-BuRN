cfbea33a95128ea14d26e0bfa6dfc9cd9b8992a6
Chuck Dries <chuck@chuckdries.com>
2018-10-20 15:33:13 -0700

    Add basscss

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,9 @@
+import 'basscss/css/basscss.min.css';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
 
 Vue.config.productionTip = false;
 
