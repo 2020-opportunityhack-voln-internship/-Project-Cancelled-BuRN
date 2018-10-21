c58f1cd1a40dc7a76622db096a1e19beafacbaee
Chuck Dries <chuck@chuckdries.com>
2018-10-21 15:33:49 -0700

    Hard code API URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.API_URL || 'http://localhost:3000/api'; // eslint-disable-line
+// export const API_URL = process.env.API_URL || 'http://localhost:3000/api'; // eslint-disable-line
+export const API_URL = 'https://ohack2018-team19.now.sh/api'; // eslint-disable-line
--- client/src/views/Report.vue
+++ client/src/views/Report.vue
@@ -19,6 +19,7 @@ export default {
     return {API_URL}
   },
   mounted() {
+    console.log(API_URL);
     this.$store.dispatch('fetchCampaigns');
   },
   computed: {
