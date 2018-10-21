3ee6bb5bf4a34c33030c004029cefcbcaf219c88
Chuck Dries <chuck@chuckdries.com>
2018-10-21 15:03:00 -0700

    Config

--- client/src/config.js
+++ client/src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.API_URL || 'http://localhost/api'; // eslint-disable-line
+export const API_URL = process.env.API_URL || 'http://localhost:3000/api'; // eslint-disable-line
