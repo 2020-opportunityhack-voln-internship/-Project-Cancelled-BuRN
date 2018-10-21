7c6366b9f5c2709302bb063ba1f17ac61b8741c4
James Quigley <james.a.quigley@asu.edu>
2018-10-21 10:49:21 -0700

    Change route prefix to /api

--- server/src/helpers/routing.helper.ts
+++ server/src/helpers/routing.helper.ts
@@ -2,6 +2,6 @@ import controllers from '../controllers';
 import { Application } from 'express';
 
 export default (app: Application) => {
-    app.use('/', new controllers.MainController(app).router);
+    app.use('/api', new controllers.MainController(app).router);
     console.log("Routing Initialized");
 }
